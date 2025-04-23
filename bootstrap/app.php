bb1218bb2f70312d06c7158206717d5fc443c333
luckynurf <lucknurfitriana17@gmail.com>
2025-04-23 15:57:01 +0700

    refactor: update schedule and middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -10,8 +11,13 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
+    ->withSchedule(function (Schedule $schedule) {
+        \Inovector\Mixpost\Schedule::register($schedule);
+    })
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->validateCsrfTokens(except: [
+            '*/payment-webhook'
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
