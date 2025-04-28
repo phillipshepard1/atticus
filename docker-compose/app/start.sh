3431b0e4bf70b9db9aa31b76973e51e10e28b067
luckynurf <lucknurfitriana17@gmail.com>
2025-04-28 08:36:25 +0000

    add config docker

---
+++ docker-compose/app/start.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+set -e
+
+role=${CONTAINER_ROLE:-queue}
+work_on_queues=${WORK_ON_QUEUES:-default}
+
+if [ "$role" = "app" ]; then
+
+    php-fpm
+
+elif [ "$role" = "queue" ]; then
+
+    echo "Running the queue..."
+    php /var/www/atticus/artisan queue:work --queue="$work_on_queues" --sleep=3 --tries=3 -v &
+
+    php /var/www/atticus/artisan queue:work --queue=publish-post --tries=10 --verbose
+
+elif [ "$role" = "scheduler" ]; then
+
+    echo "Running the scheduler..."
+    php /var/www/atticus/artisan schedule:work
+
+else
+    echo "Could not match the container role \"$role\""
+    exit 1
+fi
